e267f87a8272584f15e890529fdef426cb2404b0
L0CKCaps <capslocksoftware@hotmail.com>
2021-12-11 00:04:03 +0300

    Proje dosyası ekle.

---
+++ buyukucuk/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace buyukucuk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir sayı giriniz: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (a > 50)
+            {
+                Console.WriteLine(a + " sayısı 50'den büyük. ");
+            }
+            else if (a ==  50)
+            {
+                Console.WriteLine(a + " = 50  ");
+            }
+            else if  (a < 50)       
+            {
+                Console.WriteLine(a + " sayısı 50'den küçük. ");
+            }
+        }
+    }
+}
